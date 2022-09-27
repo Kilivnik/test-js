ce12d3ff05db9e3435b94ef23e2e1f3d35b39cfe
Kilivnik <ekstasy1990@gmail.com>
2022-09-28 00:30:34 +0300

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -65,3 +65,11 @@
 //     [3, 9],
 //   ])
 // ); // 54
+
+const first = [1, 3, 3, 4, 6, 5, 4];
+const second = [6, 3, 5, 2, 2];
+
+// Implement a logic that finds difference between "first" and "secord" lists
+// and prints the result to the console:
+// [1, 2, 4]
+// найти разницу между первым и вторым
