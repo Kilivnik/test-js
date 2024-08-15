1b4a9d5a6ee1b4d211df4ca79976b7fc2f2692d7
Kilivnik <ekstasy1990@gmail.com>
2024-08-15 14:06:33 +0300

    add new task

--- js/script1.js
+++ js/script1.js
@@ -466,3 +466,22 @@ console.log(Math.floor(Math.random() * (max - min + 1)) + min); // 50 - числ
 // console.log(cutIt(["abc", "defgh", "ijklmn"])); // ["abc","def","ijk"]
 // console.log(cutIt(["codewars", "javascript", "java"])); // ["code","java","java"]
 // console.log(5);
+
+const products = [
+  { name: "Radar", price: 1300, quantity: 4 },
+  { name: "Scanner", price: 2700, quantity: 3 },
+  { name: "Droid", price: 400, quantity: 7 },
+  { name: "Grip", price: 1200, quantity: 9 },
+];
+
+function calculateTotalPrice(productName) {
+  for (const prod of products) {
+    if (prod.name === productName) {
+      return prod.price * prod.quantity;
+    }
+  }
+  return 0;
+}
+
+console.log(calculateTotalPrice("Radar"));
+console.log(calculateTotalPrice("Blaster"));
